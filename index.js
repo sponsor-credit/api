68e006d3c487f44b8c531e38953a72485aecd1d3
HarjyotS <62819816+HarjyotS@users.noreply.github.com>
2021-08-06 17:47:47 -0700

    Github Sponsors data api endpoint

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+require("dotenv").config();
 const ghSponsors = require("./sponsor-sources/github-sponsors");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/api/v1/:name", (req, res) => {
+  var json = ghSponsors(req.params.name).then(
+    (data) => {
+      res.send(data);
+    },
+    (err) => {
+      console.log(err);
+      res.status(500).end({ err: "ran into an issue" });
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- sponsor-sources/github-sponsors.js
+++ sponsor-sources/github-sponsors.js
@@ -1,3 +1,41 @@
-module.exports = () => {
-  console.log("Hey");
-};
+const axios = require("axios");
+const process = require("process");
+
+module.exports = (username) =>
+  new Promise((resolve, reject) => {
+    console.log(username);
+    let config = {
+      headers: {
+        Authorization: `bearer ${process.env.GH_TOKEN}`,
+      },
+    };
+    axios
+      .post(
+        "https://api.github.com/graphql",
+        {
+          query: `query {
+        user(login: "${username}") {
+          ... on Sponsorable {
+            sponsors(first: 100) {
+              totalCount
+              nodes {
+                ... on User { login, avatarUrl }
+                ... on Organization { login }
+              }
+            }
+          }
+        }
+      }
+      `,
+        },
+        config
+      )
+      .then(
+        (resp) => {
+          resolve(resp.data.data.user.sponsors.nodes);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+  });
